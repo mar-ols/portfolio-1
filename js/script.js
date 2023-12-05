da05ec7673a4ade8bcae7d6c6dce571163ec7922
Marte <125874444+mar-ols@users.noreply.github.com>
2023-12-05 17:19:39 +0100

    Working on index

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function toggleSections() {
+  document.addEventListener("DOMContentLoaded", () => {
+    const toggleBtns = document.querySelectorAll(".section-btn");
+    const projectBtn = document.querySelector(".project-btn");
+    const aboutBtn = document.querySelector(".about-btn");
+    const projectSection = document.querySelector(".left-section");
+    const aboutSection = document.querySelector(".right-section");
+
+    aboutSection.style.display = "none";
+
+    toggleBtns.forEach((toggleBtn, index) => {
+      toggleBtn.addEventListener("click", () => {
+        if (index === 0) {
+          projectSection.style.display = "block";
+          aboutSection.style.display = "none";
+          aboutBtn.style.textDecoration = "line-through";
+          projectBtn.style.textDecoration = "none";
+        } else {
+          projectSection.style.display = "none";
+          aboutSection.style.display = "block";
+          aboutBtn.style.textDecoration = "none";
+          projectBtn.style.textDecoration = "line-through";
+        }
+      });
+    });
+  });
+}
+
+toggleSections();
